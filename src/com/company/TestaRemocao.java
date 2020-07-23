c4c615700538c88ec28b3e813b10fbbb6975671c
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-07-23 17:07:27 -0300

    Adicionada classe para testar comandos SQL para remoção de dados (DELETE) e suas saídas, via linha de código Java

---
+++ src/com/company/TestaRemocao.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaRemocao
+{
+    public static void main(String[] args) throws SQLException
+    {
+        ConnectionFactory factory = new ConnectionFactory();
+        Connection connection = factory.recuperarConexao();
+
+        Statement stm = connection.createStatement();
+        stm.execute("DELETE FROM PRODUTO WHERE ID > 2");
+
+        //quantas linhas foram modificadas após o uso do statement
+        Integer linhasModificadas = stm.getUpdateCount();
+
+        System.out.println("Quantidade de linhas que foram modificadas: " + linhasModificadas);
+    }
+}
