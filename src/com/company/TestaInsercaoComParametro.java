a48041507a1eebec3be461bcb0be99c02d627f19
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-07-27 18:17:20 -0300

    foi explicitado no código o commit, para o caso de sucesso na transação e o rollback para caso de erro.

--- src/com/company/TestaInsercaoComParametro.java
+++ src/com/company/TestaInsercaoComParametro.java
@@ -8,23 +8,35 @@ public class TestaInsercaoComParametro
     {
         ConnectionFactory factory = new ConnectionFactory();
         Connection connection = factory.recuperarConexao();
-        connection.setAutoCommit(false);
-        PreparedStatement stm = connection.prepareStatement
-                ("INSERT INTO PRODUTO (nome, descricao) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS);
 
-        adicionarVariavel("SmartTV", "45 polegadas", stm);
-        adicionarVariavel("Radio", "Radio de Bateria",stm);
-        return;
+        connection.setAutoCommit(false);
+
+        try {
+            PreparedStatement stm = connection.prepareStatement
+                    ("INSERT INTO PRODUTO (nome, descricao) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS);
+            adicionarVariavel("SmartTV", "45 polegadas", stm);
+            adicionarVariavel("Radio", "Radio de Bateria",stm);
+
+            connection.commit();
+
+            stm.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("ROLLBACK EXECUTADO");
+            connection.rollback();
+        }
+
     }
 
     private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
         stm.setString(1, nome);
         stm.setString(2, descricao);
 
-        /*if (nome.equals("Radio"))
+        if (nome.equals("Radio"))
         {
             throw new RuntimeException("Não foi possível adicionar o produto");
-        }*/
+        }
 
         stm.execute();
 
