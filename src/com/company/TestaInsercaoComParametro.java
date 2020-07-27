556c504c60604d91c7a6b6c7295f769ae99005e2
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-07-27 18:47:02 -0300

    modificação do código utilizando try-with-resources, que torna desnecessário explicitar o close para fechar os statements, pelo fato deles estenderem AutoCloseable

--- src/com/company/TestaInsercaoComParametro.java
+++ src/com/company/TestaInsercaoComParametro.java
@@ -7,46 +7,48 @@ public class TestaInsercaoComParametro
     public static void main(String[] args) throws SQLException
     {
         ConnectionFactory factory = new ConnectionFactory();
-        Connection connection = factory.recuperarConexao();
+        try (Connection connection = factory.recuperarConexao())
+        {
+            connection.setAutoCommit(false);
 
-        connection.setAutoCommit(false);
+            try (PreparedStatement stm = connection.prepareStatement
+                    ("INSERT INTO PRODUTO (nome, descricao) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS))
+            {
+                adicionarVariavel("SmartTV", "45 polegadas", stm);
+                adicionarVariavel("Radio", "Radio de Bateria", stm);
 
-        try {
-            PreparedStatement stm = connection.prepareStatement
-                    ("INSERT INTO PRODUTO (nome, descricao) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS);
-            adicionarVariavel("SmartTV", "45 polegadas", stm);
-            adicionarVariavel("Radio", "Radio de Bateria",stm);
+                connection.commit();
 
-            connection.commit();
-
-            stm.close();
-            connection.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("ROLLBACK EXECUTADO");
-            connection.rollback();
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("ROLLBACK EXECUTADO");
+                connection.rollback();
+            }
         }
 
     }
+            private static void adicionarVariavel (String nome, String descricao, PreparedStatement stm) throws
+            SQLException
+            {
+                stm.setString(1, nome);
+                stm.setString(2, descricao);
 
-    private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
-        stm.setString(1, nome);
-        stm.setString(2, descricao);
+                if (nome.equals("Radio"))
+                {
+                    throw new RuntimeException("Não foi possível adicionar o produto");
+                }
 
-        if (nome.equals("Radio"))
-        {
-            throw new RuntimeException("Não foi possível adicionar o produto");
-        }
+                stm.execute();
 
-        stm.execute();
+                try (ResultSet rst = stm.getGeneratedKeys())
+                {
+                    while (rst.next())
+                    {
+                        Integer id = rst.getInt(1);
+                        System.out.println("O ID criado foi: " + id);
+                    }
+                }
+            }
 
-        ResultSet rst = stm.getGeneratedKeys();
-        while (rst.next())
-        {
-            Integer id = rst.getInt(1);
-            System.out.println("O ID criado foi: " + id);
-
-        }
-        rst.close();
     }
-}
+
