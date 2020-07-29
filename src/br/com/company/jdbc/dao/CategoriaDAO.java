cba15502e357d010ba710c16542f65f2eec2e37d
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-07-29 19:47:16 -0300

    criada classe CategoriaDAO, responsável pela recuperação da conexão e da implementação da listagem de categorias cadastradas

--- src/br/com/company/jdbc/dao/CategoriaDAO.java
+++ src/br/com/company/jdbc/dao/CategoriaDAO.java
@@ -1,4 +1,41 @@
 package br.com.company.jdbc.dao;
 
-public class CategoriaDAO {
+import br.com.company.jdbc.modelo.Categoria;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoriaDAO
+{
+    private Connection connection;
+
+    public CategoriaDAO(Connection connection)
+    {
+        this.connection = connection;
+    }
+
+    public List<Categoria> listar() throws SQLException {
+        List<Categoria> categorias = new ArrayList<>();
+
+        String sql = "SELECT ID, NOME FROM CATEGORIA";
+
+        try(PreparedStatement pstm = connection.prepareStatement(sql))
+        {
+            pstm.execute();
+
+            try(ResultSet rst = pstm.getResultSet())
+            {
+                while (rst.next())
+                {
+                    Categoria categoria = new Categoria(rst.getInt(1), rst.getString(2));
+
+                    categorias.add(categoria);
+                }
+            }
+        }
+        return categorias;
+    }
 }
