9bc95ba092f315bce99c9ea7ee4bc5278a1fbe02
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-07-29 19:48:24 -0300

    Criada a classe de modelo para a Categoria de produtos

--- src/br/com/company/jdbc/modelo/Categoria.java
+++ src/br/com/company/jdbc/modelo/Categoria.java
@@ -1,4 +1,18 @@
 package br.com.company.jdbc.modelo;
 
-public class Categoria {
+public class Categoria
+{
+    private Integer id;
+    private String nome;
+
+    public Categoria(Integer id, String nome)
+    {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public String getNome()
+    {
+        return nome;
+    }
 }
