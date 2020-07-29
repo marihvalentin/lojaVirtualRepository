f1a6ac2cbf7db26fc72ebd75066c3fba4533dd2a
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-07-29 18:43:14 -0300

    Implementação do método listar, o qual lista os produtos cadastrados no banco de dados

--- src/br/com/company/jdbc/dao/ProdutoDAO.java
+++ src/br/com/company/jdbc/dao/ProdutoDAO.java
@@ -3,6 +3,8 @@ package br.com.company.jdbc.dao;
 import br.com.company.jdbc.modelo.Produto;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProdutoDAO
 {
@@ -33,4 +35,29 @@ public class ProdutoDAO
             }
         }
     }
+
+    public List<Produto> listar() throws SQLException
+    {
+        List<Produto> produtos = new ArrayList<Produto>();
+
+        String sql = "SELECT ID, NOME, DESCRICAO FROM PRODUTO";
+
+        try(PreparedStatement pstm = connection.prepareStatement(sql))
+        {
+            pstm.execute();
+
+            try(ResultSet rst = pstm.getResultSet())
+            {
+                while (rst.next())
+                {
+                    Produto produto = new Produto(rst.getInt(1), rst.getString(2),
+                            rst.getString(3));
+
+                    produtos.add(produto);
+                }
+            }
+        }
+        return produtos;
+    }
+
 }
