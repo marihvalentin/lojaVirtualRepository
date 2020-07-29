8e143954669658bc4dbe4c7f09cba8673a0663c3
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-07-29 18:38:44 -0300

    refatoração no nome da classe teste e implementação do teste de listagem de produtos

--- src/br/com/company/jdbc/TestaInsercaoEListagemComProduto.java
+++ src/br/com/company/jdbc/TestaInsercaoEListagemComProduto.java
@@ -5,18 +5,19 @@ import br.com.company.jdbc.factory.ConnectionFactory;
 import br.com.company.jdbc.modelo.Produto;
 
 import java.sql.*;
+import java.util.List;
 
-public class TestaInsercaoComProduto
+public class TestaInsercaoEListagemComProduto
 {
     public static void main(String[] args) throws SQLException
     {
         Produto comoda = new Produto("Cômoda","Cômoda Vertical");
 
-        try(Connection connection = new ConnectionFactory().recuperarConexao())
-        {
+        try(Connection connection = new ConnectionFactory().recuperarConexao()) {
             ProdutoDAO produtoDAO = new ProdutoDAO(connection);
-            new ProdutoDAO(connection).salvar(comoda);
-            //Lista = persistenciaProduto.listar();
+            produtoDAO.salvar(comoda);
+            List<Produto> listaDeProdutos = produtoDAO.listar();
+            listaDeProdutos.stream().forEach(lp -> System.out.println(lp));
         }
 
         System.out.println(comoda);
