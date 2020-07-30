4dfe798d4e21d746cef208fbc16e15357a034dab
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-07-30 17:12:12 -0300

    Alteração na implementação da listagem de categorias para também buscar e mostrar os produtos das respectivas categorias

--- src/br/com/company/jdbc/TestaListagemDeCategorias.java
+++ src/br/com/company/jdbc/TestaListagemDeCategorias.java
@@ -1,8 +1,11 @@
 package br.com.company.jdbc;
 
 import br.com.company.jdbc.dao.CategoriaDAO;
+import br.com.company.jdbc.dao.ProdutoDAO;
 import br.com.company.jdbc.factory.ConnectionFactory;
 import br.com.company.jdbc.modelo.Categoria;
+import br.com.company.jdbc.modelo.Produto;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
@@ -15,7 +18,19 @@ public class TestaListagemDeCategorias
         {
             CategoriaDAO categoriaDAO = new CategoriaDAO(connection);
             List<Categoria> listaCategorias = categoriaDAO.listar();
-            listaCategorias.stream().forEach(ct -> System.out.println(ct.getNome()));
+            listaCategorias.stream().forEach(ct -> {
+                System.out.println(ct.getNome());
+                try {
+                    for (Produto produto : new ProdutoDAO(connection).buscar(ct))
+                    {
+                        System.out.println(ct.getNome() + " - " + produto.getNome());
+                    }
+                } catch (SQLException e)
+                {
+                    e.printStackTrace();
+                }
+            });
+
         }
     }
 }
