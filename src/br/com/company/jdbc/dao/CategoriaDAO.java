a642108ca67d38231e7a9a66c46a48680cc5fc94
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-07-30 17:43:31 -0300

    Implementado o método listarComProduto, com query inner join para mostrar produtos e respectivas categorias a partir do mesmo método

--- src/br/com/company/jdbc/dao/CategoriaDAO.java
+++ src/br/com/company/jdbc/dao/CategoriaDAO.java
@@ -1,6 +1,8 @@
 package br.com.company.jdbc.dao;
 
 import br.com.company.jdbc.modelo.Categoria;
+import br.com.company.jdbc.modelo.Produto;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -35,6 +37,40 @@ public class CategoriaDAO
                     Categoria categoria = new Categoria(rst.getInt(1), rst.getString(2));
 
                     categorias.add(categoria);
+                }
+            }
+        }
+        return categorias;
+    }
+
+    public List<Categoria> listarComProdutos() throws SQLException
+    {
+        Categoria ultima = null;
+        List<Categoria> categorias = new ArrayList<>();
+
+        System.out.println("Executando a query de listar categoria");
+
+        String sql = "SELECT C.ID, C.NOME, P.ID, P.NOME, P.DESCRICAO " +
+                "FROM CATEGORIA C INNER JOIN PRODUTO P " +
+                "ON C.ID = P.CATEGORIA_ID";
+
+        try(PreparedStatement pstm = connection.prepareStatement(sql))
+        {
+            pstm.execute();
+
+            try(ResultSet rst = pstm.getResultSet())
+            {
+                while (rst.next())
+                {
+                    if(ultima == null || !ultima.getNome().equals(rst.getString(2)))
+                    {
+                        Categoria categoria = new Categoria(rst.getInt(1), rst.getString(2));
+                        ultima = categoria;
+                        categorias.add(categoria);
+                    }
+                    Produto produto = new Produto(rst.getInt(3),
+                            rst.getString(4), rst.getString(5));
+                    ultima.adicionar(produto);
                 }
             }
         }
